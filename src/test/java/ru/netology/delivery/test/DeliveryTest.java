a8d675be4a14eaf2f375c4e15d8c3d903188e9a0
Helena <v0robjeva.alena@yandex.ru>
2023-07-10 21:01:03 +0300

    chore: allure1

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -63,35 +63,22 @@ class DeliveryTest {
     }
 
     @Test
-    @DisplayName("Should not successful plan meeting")
+    @DisplayName("Invalid phone number")
     void shouldNotSuccessfulPlanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 2;
+        var daysToAddForFirstMeeting = 3;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 1;
+        var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
         $("span[data-test-id=city] input").setValue(validUser.getCity());
         $("span[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("span[data-test-id=date] input").setValue(firstMeetingDate);
         $("span[data-test-id=name] input").setValue(validUser.getName());
-        $("span[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("span[data-test-id=phone] input").setValue("0 000 000 00 00");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
-        $(byText( "Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=\"success-notification\"] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
-                .shouldBe(visible);
-        $("span[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("span[data-test-id=date] input").setValue(secondMeetingDate);
-        $$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=\"replan-notification\"] .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldBe(visible);
-        $$("button").find(exactText("Перепланировать")).click();
-        $("[data-test-id= 'success-notification']")
-                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate))
-                .shouldBe(visible);
+        $(byText( "Неверный формат номера телефона!")).shouldBe(visible);
     }
 }
 
